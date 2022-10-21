053e5952e38177eb1de454390096842f2c8bc446
showyourwork <showyourwork>
2022-10-21 11:11:50 +0200

    [showyourwork] overleaf sync

--- src/scripts/merger_ratios.py
+++ src/scripts/merger_ratios.py
@@ -84,7 +84,7 @@ ax.spines['right'].set_color('darkorange')
 ax.yaxis.label.set_color('darkorange')
 ax.tick_params(axis='y', colors='orange')
 ax.set_yticklabels([])
-ax.set_ylim(-2,75)
+ax.set_ylim(-2,25)
 
 # now g7.55e11
 data = pickle.load(open(paths.data / '7.55e11_merger_tree_data.dat','rb'))
@@ -92,16 +92,15 @@ time = np.linspace(0,13.8, len(data['gas_mass']))
 
 ax1l.plot(time, data['gas_mass'], label='$\mathrm{gas/, mass}$' )
 ax1l.text(7,5e10,'g7.55e11',fontsize=30)
-ax1l.set_yscale('log')
-
 ax1 = ax1l.twinx()
+ax1l.set_yscale('log')
 ax1.plot(time[:-2],data['gas_ratio'],c='darkorange', label='$\mathrm{merger\ ratio\ gas}$')
 
 ax1.spines['right'].set_color('darkorange')
 ax1.yaxis.label.set_color('darkorange')
 ax1.tick_params(axis='y', colors='orange')
 ax1.set_yticklabels([])
-ax1.set_ylim(-2,75)
+ax1.set_ylim(-2,25)
 
 
 # now we do g7.08e11
@@ -110,33 +109,31 @@ time = np.linspace(0,13.8, len(data['gas_mass']))
 
 ax2l.plot(time, data['gas_mass'], label='$\mathrm{gas/, mass}$' )
 ax2l.text(7,5e10,'g7.08e11',fontsize=30)
-ax2l.set_yscale('log')
-
 ax2 = ax2l.twinx()
+ax2l.set_yscale('log')
 ax2.plot(time[:-1],data['gas_ratio'],c='darkorange', label='$\mathrm{merger\ ratio\ gas}$')
 
 ax2.spines['right'].set_color('darkorange')
 ax2.yaxis.label.set_color('darkorange')
 ax2.tick_params(axis='y', colors='orange')
 ax2.set_yticklabels([])
-ax2.set_ylim(-2,75)
+ax2.set_ylim(-2,25)
 
 # now we do g8.26e11
 data = pickle.load(open(paths.data / '2.79e12_merger_tree_data.dat','rb'))
 time = np.linspace(0,13.8, len(data['gas_mass']))
 
-ax2l.plot(time, data['gas_mass'], label='$\mathrm{gas/, mass}$' )
-ax2l.text(7,5e10,'g2.79e12',fontsize=30)
-ax2l.set_yscale('log')
-
-ax3 = ax2l.twinx()
+ax3l.plot(time, data['gas_mass'], label='$\mathrm{gas/, mass}$' )
+ax3l.text(7,5e10,'g2.79e12',fontsize=30)
+ax3 = ax3l.twinx()
+ax3l.set_yscale('log')
 ax3.plot(time[:-5],data['gas_ratio'],c='darkorange', label='$\mathrm{merger\ ratio\ gas}$')
 
 ax3.spines['right'].set_color('darkorange')
 ax3.yaxis.label.set_color('darkorange')
 ax3.tick_params(axis='y', colors='orange')
 ax3.set_yticklabels([])
-ax3.set_ylim(-2,75)
+ax3.set_ylim(-2,25)
 
 #plt.spines['right'].set_color('darkorange')
 
