0fd543ed884325022612ed59c0f01f085fd1296b
showyourwork <showyourwork>
2022-10-21 11:21:14 +0200

    [showyourwork] overleaf sync

--- src/scripts/merger_ratios.py
+++ src/scripts/merger_ratios.py
@@ -68,9 +68,9 @@ ax3l.set_ylim(1e7,4e11)
 #let's do g2.79e12
 
 data = pickle.load(open(paths.data / '2.79e12_merger_tree_data.dat','rb'))
-time = np.linspace(0,13.8, len(data['gas_mass']))
+time = np.linspace(0,13.8, len(data['gas_mass']))[::-1]
 
-axl.plot(time, data['gas_mass'][::-1], label='$\mathrm{gas/, mass}$' )
+axl.plot(time, data['gas_mass'], label='$\mathrm{gas/, mass}$' )
 axl.text(7,5e10,'g2.79e12',fontsize=30)
 axl.set_yscale('log')
 
@@ -87,7 +87,7 @@ ax.set_ylim(-.05,.25)
 data = pickle.load(open(paths.data / '7.55e11_merger_tree_data.dat','rb'))
 time = np.linspace(0,13.8, len(data['gas_mass']))
 
-ax1l.plot(time, data['gas_mass'][::-1], label='$\mathrm{gas/, mass}$' )
+ax1l.plot(time, data['gas_mass'], label='$\mathrm{gas/, mass}$' )
 ax1l.text(7,5e10,'g7.55e11',fontsize=30)
 ax1 = ax1l.twinx()
 ax1l.set_yscale('log')
@@ -105,7 +105,7 @@ ax1.set_ylim(-.05,.25)
 data = pickle.load(open(paths.data / '7.08e11_merger_tree_data.dat','rb'))
 time = np.linspace(0,13.8, len(data['gas_mass']))
 
-ax2l.plot(time, data['gas_mass'][::-1], label='$\mathrm{gas/, mass}$' )
+ax2l.plot(time, data['gas_mass'], label='$\mathrm{gas/, mass}$' )
 ax2l.text(7,5e10,'g7.08e11',fontsize=30)
 ax2 = ax2l.twinx()
 ax2l.set_yscale('log')
@@ -122,7 +122,7 @@ ax2.set_ylim(-.05,.25)
 data = pickle.load(open(paths.data / '2.79e12_merger_tree_data.dat','rb'))
 time = np.linspace(0,13.8, len(data['gas_mass']))
 
-ax3l.plot(time, data['gas_mass'][::-1], label='$\mathrm{gas/, mass}$' )
+ax3l.plot(time, data['gas_mass'], label='$\mathrm{gas/, mass}$' )
 ax3l.text(7,5e10,'g2.79e12',fontsize=30)
 ax3 = ax3l.twinx()
 ax3l.set_yscale('log')
