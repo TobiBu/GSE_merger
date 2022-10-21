e82be647346c8b001da442a1189ea31366127bed
Tobias Buck <buck@mpia.de>
2022-10-21 13:51:35 +0200

    latex disabled in mdf_oxygen.py

--- src/scripts/mdf_oxygen.py
+++ src/scripts/mdf_oxygen.py
@@ -37,7 +37,7 @@ plt.rcParams['legend.edgecolor'] = 'lightgray'
 plt.rcParams['patch.linewidth'] = 3
 
 plt.rcParams.update({
-    "text.usetex": True,
+    "text.usetex": False,
     "font.family": "Helvetica"
 })
 
